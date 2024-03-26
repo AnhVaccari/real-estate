8a02562e1c5f5fe239db86f68b89a46e84163b4c
Anh <anh.vaccari@orange.fr>
2024-03-26 17:59:49 +0100

    complete about page and add icon to web site title page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,26 @@
-
+/* eslint-disable react/no-unescaped-entities */
 
 export default function About() {
     return (
-        <div>
-            About
+        <div className="py-20 px-4 max-w-6xl mx-auto">
+            <h1 className="text-3xl font-bold mb-4 text-slate-800">About Amy Estate</h1>
+            <p className="mb-4 text-slate-700">
+                Amy Estate is a leading real estate agency that specializes in helping
+                clients buy, sell and rent properties in the most desirable neighborhoods.
+                <br />As a sole proprietorship, I, Amy, am dedicated to providing exceptional service
+                and ensuring a smooth process for all transactions.
+            </p>
+            <p className="mb-4 text-slate-700">
+                My mission is to assist clients in achieving their real estate goals by offering expert advice,
+                personalized service, and a deep understanding of the local market.
+                <br />Whether you're looking to buy, sell, or rent a property, I'm here to support you every step of the way.
+            </p>
+            <p className="mb-4 text-slate-700">
+                With my extensive experience and knowledge in the real estate industry,
+                I'm committed to delivering the highest level of service to my clients.
+                <br />I firmly believe that buying or selling a property should be an exciting and rewarding experience,
+                and I'm dedicated to making that a reality for each and every one of my clients.
+            </p>
         </div>
     )
 }
